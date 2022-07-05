eb25fd225e6ad63c7e4f94302e6a5c3c8c9c76c3
weaverjess <87557577+weaverjess@users.noreply.github.com>
2022-07-05 16:45:28 +0900

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -57,31 +57,37 @@ class PacmanGame():
         positions = DATA['character_positions']
         ghost_pos = positions['ghosts']
 
+
         self.pacman = Pacman(
             self.game_box,
-            COLOR.yellow,
+            # Part 2-1: Set Pacman's color to yellow, you can use the constant `COLOR.yellow`.
+            # TODO: Set Pacmans's color to yellow!
             positions['pacman']
         )
 
         self.ghosts = {
+            #　Part 2−２: Set Blinky's color to red, you can use the constant `COLOR.red`.
             'Blinky': Ghost(
                 self.game_box,
-                COLOR.red,
+                # TODO: Set Blinky's color to red!
                 ghost_pos[0]
             ),
 
+            # Part 2-3: Set Pinky's color to pink, you can use the constant `COLOR.pink`.
             'Pinky': Ghost(
                 self.game_box,
                 COLOR.pink,
                 ghost_pos[1]
             ),
 
+            # Part 2-4: Set Inky's color to cyan, you can use the constant `COLOR.cyan`.
             'Inky': Ghost(
                 self.game_box,
                 COLOR.cyan,
                 ghost_pos[2]
             ),
 
+            # Part 2-5: Set Clyde's color to orange, you can use the constant `COLOR.orange`.
             'Clyde': Ghost(
                 self.game_box,
                 COLOR.orange,
@@ -303,3 +309,181 @@ class PacmanGame():
 
 
 PacmanGame()
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+3:39
+character.py
+3:39
+import abc
+import random
+import time
+from .character_progression import CharProgression
+
+
+class Character():
+    def __init__(self, game_box, color, initial_position=None):
+        self.game_box = game_box
+
+        # Part 1-1  In `character.py` please prepare the Character class so that it can recieve a `color` attribute when instanced.
+        # TODO: Implement color
+
+        self.pass_over = False
+        self.stopped = False
+
+        self.init_progressions()
+
+        if initial_position:
+            self.set_position(initial_position)
+
+
+    @abc.abstractmethod
+    def init_progressions(self):
+        self.progressions = {}
+        return
+
+
+    def appear(self):
+        self.draw_char(self.current_progression.get_char(), False)
+
+
+    def vanish(self):
+        self.draw_char(' ', False)
+
+
+    def toggle(self, show=True):
+        if show:
+            self.appear()
+        else:
+            self.vanish()
+
+
+    def move(self, direction):
+        new_position = self.game_box.get_new_position(self.current_position, direction)
+
+        if new_position != self.current_position:
+            self.stopped = False
+            self.update_progression(direction)
+            self.set_position(new_position)
+        else:
+            self.stopped = True
+
+
+    def set_position(self, coordinates):
+        if hasattr(self, 'current_position'):
+            if not self.pass_over:
+                self.draw_char(' ')
+            else:
+                self.draw_char(' ', True, True)
+
+        self.current_position = coordinates
+        self.draw_char(self.current_progression.get_char(), False)
+
+
+    def update_progression(self, direction):
+        self.current_progression = self.progressions.get(direction)
+        self.current_progression.update()
+
+
+    def draw_char(self, char, update=True, redraw=False):
+        y, x = self.current_position
+
+        color = self.color
+
+        if redraw:
+            char = self.game_box.map_matrix[y][x]
+            color = self.game_box.color_matrix[y][x]
+
+        self.game_box.map_box.addstr(
+            y,
+            x,
+            char,
+            color
+        )
+
+        if update:
+            self.game_box.map_matrix[y][x] = char
+
+
+
+
+class Pacman(Character):
+    def init_progressions(self):
+        self.progressions = {
+            'UP': CharProgression('UP', ['v', 'V', '|', '|', 'V', 'v']),
+            'DOWN': CharProgression('DOWN', ['^']),
+            'LEFT': CharProgression('LEFT', ['}', ')', '>', '-', '-', '>', ')', '}']),
+            'RIGHT': CharProgression('RIGHT', ['{', '(', '<', '-', '-', '<', '(', '{'])
+        }
+
+        self.current_progression = self.progressions.get('RIGHT')
+
+
+    # Part 3: Implement Pacman death animation
+    def die(self):
+        self.appear()
+        # 1. Loop through the array `['O', 'o', '.', "'", '*', ' ']` to perform the character changes that act as our animations.
+        # 2. Pause on every iteration of the loop for `0.2` seconds using `time.sleep()`
+        # 3. Use the `self.draw_char()` method to draw the character in the animation sequence, the second parameter is `False`.
+        # 4. Finally, refresh the game_box/main screen with `self.game_box.map_box.refresh()`.
+
+        self.vanish()
+
+
+    def respawn(self):
+        self.appear()
+
+
+
+
+class Ghost(Character):
+    SPEED_DAMPER_LEVEL = 1
+
+    def __init__(self, *args, **kwargs):
+        super(Ghost, self).__init__(*args, **kwargs)
+        # Part 1-2: In `character.py`, for Ghosts please set override the default value of false for the `pass_over` attribute to `True`
+        self.pass_over = True
+        self.wait_flag = 0
+
+
+    def init_progressions(self):
+        self.progressions = {
+            'UP': CharProgression('UP', ['M']),
+            'DOWN': CharProgression('DOWN', ['M']),
+            'LEFT': CharProgression('LEFT', ['M']),
+            'RIGHT': CharProgression('RIGHT', ['M'])
+        }
+
+        self.current_progression = self.progressions.get('RIGHT')
+
+
+    def move_in_random_direction(self, bias=None):
+        if self.wait_flag < self.SPEED_DAMPER_LEVEL:
+            self.wait_flag += 1
+            return
+
+        self.wait_flag = 0
+
+        box = self.game_box
+
+        current_direction = self.current_progression.name
+        forward_directions = box.get_all_forward_directions(current_direction)
+        possible_directions = []
+
+        for d in forward_directions:
+            if box.get_new_position(self.current_position, d) != self.current_position:
+                possible_directions.append(d)
+
+        if possible_directions:
+            self.move(random.choice(possible_directions))
+        else:
+            self.move(box.get_opposite_direction(current_direction))
+
+
+    def respawn(self):
+        self.appear()
